17dc8809149e67a32132f349ecd4236a95d98ea7
DeAndre Johnson <dre@phreshr.com>
2019-10-15 04:43:44 +0800

    Turned Saved Movie Links to NavLinks

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const SavedList = props => (
   <div className='saved-list'>
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <span key={movie.id} className='saved-movie'>
-        <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+        <NavLink
+          activeStyle={{
+            fontWeight: 'bold',
+            color: 'indianred',
+          }}
+          to={`/movies/${movie.id}`}
+        >
+          {movie.title}
+        </NavLink>
       </span>
     ))}
     <div className='home-button'>
